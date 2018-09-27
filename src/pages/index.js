49c35f1183524e5b698a000d6081ba4b43f8aaea
Nnadozie <onwukahnr2@gmail.com>
2018-09-27 09:50:55 +0100

    you

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
     <h1>Learn to design and code React app</h1>
     <p>Complete courses about the best tools and design 
       systems. prototype and build app with React and Swift</p>
-    <Link to="/page-2/">Watch the video 2</Link>
+    <Link to="/page-2/">fuck you 2</Link>
   </Layout>
 )
 
