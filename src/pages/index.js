b2ce8716d918244e03a14e84f0bacdf4588d8fa9
Nnadozie <onwukahnr2@gmail.com>
2018-09-27 09:44:21 +0100

    my first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Learn to design and code React app</h1>
+    <p>Complete courses about the best tools and design 
+      systems. prototype and build app with React and Swift</p>
+    <Link to="/page-2/">Watch the video 2</Link>
+  </Layout>
+)
+
+export default IndexPage
